ed35aa142df8c1e6a1a7fd08b7e4cc30c9f2cc81
Two J <TwoJ@gmail.com>
2023-07-20 17:11:46 +0630

    fixed-key

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 export const config = {
   sanityToken: process.env.NEXT_PUBLIC_SANITY_TOKEN || "",
   stripeKey: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || "",
-  stripeSecret: process.env.STRIPE_SECRET_KEY || "",
+  stripeSecret: process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY || "",
 };

3dee650d2a226a7cfe5398beab59b6802c418c4a
Two J <TwoJ@gmail.com>
2023-07-20 17:10:10 +0630

    add-stripe-secret-key

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,7 +1,7 @@
 import { config } from "../../config";
 import Stripe from "stripe";
 
-const stripe = new Stripe(config.stripeKey);
+const stripe = new Stripe(config.stripeSecret);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {

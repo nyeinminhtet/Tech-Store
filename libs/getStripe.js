bcf11e804c7cc12868dcafe0955084ed1c2d0a87
Two J <TwoJ@gmail.com>
2023-07-21 09:37:34 +0630

    fixed-dispaly

--- libs/getStripe.js
+++ libs/getStripe.js
@@ -5,7 +5,7 @@ let stripePromise;
 
 const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(config.stripePublishableKey);
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
   }
 
   return stripePromise;
--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,7 +1,7 @@
 import { config } from "../../config";
 import Stripe from "stripe";
 
-const stripe = new Stripe(config.stripeSecret);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
